2a2501a8d56d131d8139e6f882164d4a1acf63e4
Thomas Devine <tomo@thomass-imac.home>
2018-02-25 21:53:49 +0000

    Exercise 5 & 6 (GAA)

---
+++ gaa2013/getData.php
@@ -0,0 +1,20 @@
+<?php
+
+// This program returns the results in JSON format
+// To see the output use the URL:  http://localhost/Underscore/getData.php
+
+
+$connection = mysqli_connect("localhost","root","");
+mysqli_select_db($connection,"gaa2013");
+$result = mysqli_query($connection,"select gameweek,datetime,description,team1,team2,team1gls,team1pts,team2gls,team2pts from games,categories where games.category=categories.id order by datetime");
+
+$rs = array();
+$i=0;
+while($rs[] = mysqli_fetch_assoc($result)) {
+// do nothing ;-)
+}
+mysqli_close($connection);
+
+unset($rs[count($rs)-1]);  //removes a null value
+print(json_encode($rs, JSON_NUMERIC_CHECK));
+?>
